61289443dfd9d2ca41c8a00e3bb00420c3eaf179
Kat <lian.kaitlin0@gmail.com>
2025-07-28 13:09:32 -0400

    created new view and created UI for ContentView and NewToDoView

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack() {
+            HStack() {
+                Text("To-Do List")
+                    .font(.system(size: 40))
+                    .fontWeight(.black)
+                Spacer()
+                Button{
+                    
+                } label: {
+                    Text("+")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+            }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
